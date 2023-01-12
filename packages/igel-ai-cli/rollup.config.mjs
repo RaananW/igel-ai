75069ffa4e6e5a4b1b7de8883f873df46588098a
Raanan Weber <Raanan.Weber@microsoft.com>
2023-01-12 19:37:07 +0100

    cli support

--- packages/igel-ai-cli/rollup.config.mjs
+++ packages/igel-ai-cli/rollup.config.mjs
@@ -1,13 +1,19 @@
 import typescript from "@rollup/plugin-typescript";
+import copy from "@guanghechen/rollup-plugin-copy";
 
 export default [
     {
         input: "src/cliInterface.ts",
         output: {
-            file: "dist/cliInterface.mjs",
+            file: "bin/cliInterface.mjs",
             format: "esm",
             sourcemap: true,
         },
-        plugins: [typescript({ sourceMap: true, inlineSources: true })],
+        plugins: [
+            typescript({ sourceMap: true, inlineSources: true }),
+            copy({
+                targets: [{ src: "src/cli.mjs", dest: "bin" }],
+            }),
+        ],
     },
 ];
